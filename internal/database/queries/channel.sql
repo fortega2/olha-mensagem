345d656e8965c60bd77824ec9c4f1f1e0baed4d8
Facundo Ernesto Ortega <facuortega21@gmail.com>
2025-09-05 09:05:02 -0300

    feat: enhance channel management by adding username to channel details and updating response structures

--- internal/database/queries/channel.sql
+++ internal/database/queries/channel.sql
@@ -14,14 +14,24 @@ ORDER BY
     c.id DESC;
 
 -- name: GetChannelByID :one
-SELECT *
-FROM channels
-WHERE id = ?;
+SELECT
+    c.id,
+    c.name,
+    c.description,
+    c.created_by,
+    u.username AS created_by_username,
+    c.created_at
+FROM
+    channels AS c
+INNER JOIN
+    users AS u ON u.id = c.created_by
+WHERE
+    c.id = ?;
 
 -- name: CreateChannel :one
 INSERT INTO channels (name, description, created_by)
 VALUES (?, ?, ?)
-RETURNING *;
+RETURNING id;
 
 -- name: DeleteChannel :exec
 DELETE FROM channels
